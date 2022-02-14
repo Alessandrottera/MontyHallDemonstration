1ed1e0aba036a983de95b8bee766216190482257
Alessandro <alessandro.zanzottera13@gmail.com>
2022-02-14 11:28:45 +0100

    Percentage log fix

--- src/montyHall/PlayTheGame/PlayerPlaysRandom.java
+++ src/montyHall/PlayTheGame/PlayerPlaysRandom.java
@@ -1,5 +1,6 @@
 package montyHall.PlayTheGame;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class PlayerPlaysRandom {
@@ -24,11 +25,17 @@ public class PlayerPlaysRandom {
             switchWins += doors[3 - choice - shown];
         }
 
-        int switchWinsPercentage = (switchWins * 100) / rounds;
-        int stayWinsPercentage = (stayWins * 100) / rounds;
+        String switchWinsPercentage = percentageCalculator(switchWins, rounds);
+        String stayWinsPercentage = percentageCalculator(stayWins, rounds);
 
         System.out.println("Game was played " + rounds + " times");
         System.out.println("User had the opportunity of winning by switching " + switchWins + " times. If you switch your door, your percentage of winning is: " + switchWinsPercentage + "%");
         System.out.println("User had the opportunity of winning by not switching " + stayWins + " times. If you don't switch your door, your percentage of winning is: " + stayWinsPercentage + "%");
+    }
+
+    private String percentageCalculator(int wins, int rounds){
+        double result = ((double)wins * 100.0) / (double)rounds;
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+        return numberFormat.format(result);
     }
 }
