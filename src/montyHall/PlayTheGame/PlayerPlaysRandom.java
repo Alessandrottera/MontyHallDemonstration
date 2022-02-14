f2a622ec4660acfde28dbddfd81e64b0ef6dbf4a
alessandro.zanzotter <alessandro.zanzottera@csttech.it>
2022-02-14 16:37:27 +0100

    randomized player fixes

--- src/montyHall/PlayTheGame/PlayerPlaysRandom.java
+++ src/montyHall/PlayTheGame/PlayerPlaysRandom.java
@@ -1,29 +1,19 @@
 package montyHall.PlayTheGame;
 
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class PlayerPlaysRandom {
 
     public void startMontyHallGameRandomized() {
-        int switchWins = 0;
-        int stayWins = 0;
+
         int rounds = 100000;
-        Random gen = new Random();
+        Map<String, Integer> results = playTheGame(rounds);
 
-        for (int plays = 0; plays < rounds; plays++) {
-            int[] doors = {0, 0, 0};
-            doors[gen.nextInt(3)] = 1;
-            int choice = gen.nextInt(3);
-            int shown;
-
-            do {
-                shown = gen.nextInt(3);
-            }
-            while (doors[shown] == 1 || shown == choice);
-            stayWins += doors[choice];
-            switchWins += doors[3 - choice - shown];
-        }
+        int switchWins = results.get("switchWins");
+        int stayWins = results.get("stayWins");
 
         String switchWinsPercentage = percentageCalculator(switchWins, rounds);
         String stayWinsPercentage = percentageCalculator(stayWins, rounds);
@@ -33,8 +23,35 @@ public class PlayerPlaysRandom {
         System.out.println("User had the opportunity of winning by not switching " + stayWins + " times. If you don't switch your door, your percentage of winning is: " + stayWinsPercentage + "%");
     }
 
-    private String percentageCalculator(int wins, int rounds){
-        double result = ((double)wins * 100.0) / (double)rounds;
+    private Map<String, Integer> playTheGame(int rounds) {
+
+        Map<String, Integer> results = new HashMap<>();
+        Integer switchWins = 0;
+        Integer stayWins = 0;
+        Random humanInput = new Random();
+
+        for (int plays = 0; plays < rounds; plays++) {
+            int[] doors = {0, 0, 0};
+            doors[humanInput.nextInt(3)] = 1;
+            int choice = humanInput.nextInt(3);
+            int montysDoorChoice;
+
+            do {
+                montysDoorChoice = humanInput.nextInt(3);
+            }
+            while (doors[montysDoorChoice] == 1 || montysDoorChoice == choice);
+
+            stayWins += doors[choice];
+            switchWins += doors[3 - choice - montysDoorChoice];
+        }
+
+        results.put("stayWins", stayWins);
+        results.put("switchWins", switchWins);
+        return results;
+    }
+
+    private String percentageCalculator(int wins, int rounds) {
+        double result = (wins * 100.0) / rounds;
         DecimalFormat numberFormat = new DecimalFormat("#.00");
         return numberFormat.format(result);
     }
